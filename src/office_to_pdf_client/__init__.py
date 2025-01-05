eb93a74e84729a669ea36858880a53ff119374f1
Nagayama Takahiro <nagayamat5601@gmail.com>
2025-01-06 00:24:01 +0900

    fix:initでクラス指定

---
+++ src/office_to_pdf_client/__init__.py
@@ -0,0 +1,7 @@
+from office_to_pdf_client._client import OfficeToPdfClient
+from office_to_pdf_client._convert.responses import SingleFileResponse
+
+__all__ = [
+    "OfficeToPdfClient",
+    "SingleFileResponse"
+]
